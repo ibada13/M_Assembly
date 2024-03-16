efc2f54abe72674ae317d891a43a4e7bd8dbfa62
hunter666hz <hzhunter7@gmail.com>
2024-03-16 22:07:54 +0100

    cmpl

--- Assembly/main.asm
+++ Assembly/main.asm
@@ -3,15 +3,18 @@ include C:\masm32\include\masm32rt.inc
 .stack 4096
 .data
 
-prompt db "hello wrold: ",0
+prompt db "hello world: ",0
 del db "world",0
+dell db "world",0
+
 minput DB 50 DUP(?)
 slen db 0
 .code 
 ;
 ;1 str => size at cx
 strlen proc
-mov eax , [esp + 4 ]
+push eax
+mov eax , [esp + 8 ]
 cmp byte ptr [eax],0
 je retl
 xor cx,cx
@@ -23,14 +26,33 @@ inc eax
 cmp byte ptr [eax],0
 jne l1
 retl:
-
+pop eax
 ret
 
 strlen endp
-;1 should be included in 2
+strcmp proc
+push ebx
+mov  eax,[esp + 12]
+mov  ebx,[esp+8]
+l1:
+mov dl,[eax]
+cmp dl,[ebx]
+jne retl
+cmp dl,0
+je retl
+inc eax
+inc ebx 
+jmp l1
+retl:
+pop ebx
+ret
+strcmp endp
+
+
+;1 should be included in 2: the adress of 1 that si in 2 will be in ebx else cx is 0 
 checkstrinstr proc
-mov eax,[esp + 4 ]
-mov ebx,[esp + 8 ]
+mov eax,[esp + 4 ];1
+mov ebx,[esp + 8 ];2
 push ebp
 mov ebp,esp
 
@@ -39,38 +61,44 @@ sub esp ,8
 push eax
 call strlen
 mov word ptr [ebp-4],cx
+cmp cx,0 
+jle retl
 add esp,4
 push ebx
 call strlen
 mov word ptr [ebp-8],cx
-
+cmp cx,0 
+jle retl
 add esp,4
+mov dl,[eax]
+l1:
 cmp cx,[ebp - 4 ]
 jl retl
-l1:
 
-mov edx,[eax]
-cmp edx,[ebx]
+cmp dl,[ebx]
 jne l2
-dec cx
-cmp cx,0
-je retl
-inc ebx
+push dx
+push cx
+push eax
+push ebx
 
+call strcmp
+mov eax,[esp + 4]
+mov cx,[esp+ 8]
+add esp , 8
+cmp dl,0 
+je retl
+
+mov dl,[esp]
 
 
 l2:
-inc ax
-mov dx,[ebp - 4]
-dec dx
-mov [ebp - 4],dx
-cmp cx,dx
-jl retl
+inc ebx
+dec cx
 jmp l1
 ;push offset zmesg
 ;call StdOut
 
-zl:
 retl:
 mov esp,ebp
 pop ebp
@@ -78,31 +106,57 @@ ret
 checkstrinstr endp
 ;p3 = the one we want to remove ,p2= the target , p1 destnition
 removestr proc
-pop eax ; rm
 
+pop eax ; rm
 pop ebx ; tr
 pop edx ; des
-l3:
-cmp byte ptr [ebx],0
-je l2
-cmp byte ptr [eax],0
-je l2
-
+push ebp 
+mov ebp,esp
+sub esp,12
+push eax
+call strlen
+mov [ebp - 4 ],cx
+push edx
+call strlen
+mov [ebp - 12 ],cx
+push ebx
+call strlen
+mov [ebp - 8 ],cx
+cmp cx,[ebp - 4]
+jl retl
+sub cx,[ebp - 4]
+cmp cx,[ebp - 12]
+jl retl
+cmp [ebp - 4],0
+jle retl
+mov cl,[eax]
 l1:
+cmp [ebx],cl
+jne l2
 
 
-
-
-cmp byte ptr [ebx],0
-jne l3
-cmp byte ptr [edx],0
-jne l3
 l2:
+inc ebx
 
+push ebx 
+push eax
+call checkstrinstr
+cmp cx,0
+je retl:
+
+
+
+
+retl:
+mov esp,ebp
+pop ebp
+ret
 removestr endp
 start: 
-    push offset prompt
+    push offset prompt ; p>d
     push offset del
-    call checkstrinstr    
+    call checkstrinstr   
+    push ebx 
+    call StdOut
     invoke ExitProcess,1
 end start
